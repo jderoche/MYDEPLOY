c67d6713c9538ce693c9ea2e53595b24e46e3272
Joel Deroche <jederaliz@gmail.com>
2018-03-07 11:56:02 +0100

    Update with new batch and front end update

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+global.FileData = "aerar";
+
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', function (msg,res) {
+        console.log("No ERROR");
+        
+        var data = {
+        firstName: 'deroche '+Date()+': Build Done, Test Report :\n'+msg,
+        progress: 70,
+        welcome:"All done Ok"
+        };
+        res.send(data);
+      
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,13 +37,56 @@ app.get('/todo', function(req,res) {
 
     bat.on('exit', (code) => {
     console.log(`Child exited with code ${code}`);
-    var data = {
-      firstName: 'John User : Build Task Complete at : 70%',
-      progress: 70,
-      welcome:"salue"
-    };
+    var directoryPath = 'CCodeParser\\TestResults';
+    console.log(directoryPath);
 
-    res.send(data);
+    
+    fs.readdir(directoryPath, function (err, files) 
+    {
+        //handling error
+        if (err) {
+            console.error( "Error stating file.", err );
+            return;
+        }
+        else
+        {
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            var patt = new RegExp("(trx)+");
+            var fileextvalid = patt.test(file);
+            if (fileextvalid)
+            {
+              console.log('./CCodeParser/TestResults/'+file);
+              fs.readFile( './CCodeParser/TestResults/'+file,'utf8', function(err, datafile) {
+                if (err)
+                {
+                  eventEmitter.emit('scream',"-Error-",res);
+                  return;
+                }
+                else
+                {
+                  global.FileData = datafile.substr(datafile.indexOf("<Counters")+9);
+                  global.FileData = global.FileData.substr(1,global.FileData.indexOf("/>")-1).split(" ");
+                  fLen = global.FileData.length;
+                  var datatext = "";
+                  for (i = 0; i < fLen; i++) {
+                      datatext += global.FileData[i] + "\n";
+                  }
+                  console.log(datatext); 
+                  //Fire the 'scream' event:
+                  eventEmitter.emit('scream',datatext,res);
+                }
+
+              });
+            }
+
+        });
+        }
+    });
+    
+
+
     });
 
 })
--- my.bat
+++ my.bat
@@ -1,3 +1,18 @@
-echo "test"
-ping www.google.com
-echo "end"
+rmdir /Q /S CCodeParser
+del /Q RunTest.xml
+echo off
+echo " ______________________________________"
+echo "| E2CAD AUTO DEPLOY TOOLS SYSTEM v1.00 |"
+echo "| Node JS v8.9.4                       |"
+echo "|______________________________________|"
+echo ""
+git clone https://github.com/jderoche/CCodeParser.git
+cd CCodeParser
+build.bat
+echo off
+echo ""
+echo " _______________________________"
+echo "|                               |"
+echo "| Build And Test Finished....   |"
+echo "|_______________________________|"
+
